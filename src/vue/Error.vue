f9c121bef4ddb32202545cc75570aefcccaeb760
ESR <82104698+esrdapp@users.noreply.github.com>
2021-09-29 16:30:47 +0100

    Update Error.vue

--- src/vue/Error.vue
+++ src/vue/Error.vue
@@ -8,8 +8,8 @@
         <div v-if="error === 'insecure_location'">
             <h3>Security alert</h3>
 
-            You are using Vanity-ETH from an unknown website, which could steal your private keys.<br>
-            To stay safe, use Vanity-ETH on <a href="https://vanity-eth.tk" target="_blank">vanity-eth.tk</a>, or
+            You are using Vanity-HPB from an unknown website, which could steal your private keys.<br>
+            To stay safe, use Vanity-HPB on <a href="https://vanity-eth.tk" target="_blank">vanity-eth.tk</a>, or
             download the latest build <a href="https://git.io/veth-dl" target="_blank">here</a> to use offline.
         </div>
         <p v-else v-html="error.replace('\n', '<br>')"></p>
