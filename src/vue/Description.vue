8bc0a66c193d5ca3dad7375be01cca82f081166d
ESR <82104698+esrdapp@users.noreply.github.com>
2021-09-29 16:29:57 +0100

    Update Description.vue

--- src/vue/Description.vue
+++ src/vue/Description.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="panel">
         <p>
-            Vanity-ETH is an open source tool using your web browser to generate Ethereum vanity addresses.<br>
+            Vanity-HPB is an open source tool using your web browser to generate HPB vanity addresses.<br>
             Enter a short prefix/suffix of your choice, and click ‘generate’ to start.
         </p>
         <div class="shortcut">
@@ -32,26 +32,23 @@
             <b>Vanity-ETH cannot and will never store your private key</b>, and if you don't trust it, you have 3 ways to ensure
             your key remains private:<br>
             &nbsp;-&nbsp;Once the web page is loaded, you can turn off the internet and continue playing, it will work seamlessly<br>
-            &nbsp;-&nbsp;You can also download the latest build of Vanity-ETH
+            &nbsp;-&nbsp;You can also download the latest build of Vanity-HPB
             <a href="https://git.io/veth-dl" target="_blank">here</a> and use it on a completely offline computer<br>
             &nbsp;-&nbsp;The code is 100% open source and available on
-            <a href="https://github.com/bokub/vanity-eth" target="_blank">Github</a>. You can review it as much as you want before using it<br>
+            <a href="https://github.com/esrdapp/vanity-eth" target="_blank">Github</a>. You can review it as much as you want before using it<br>
             <br>
-            Vanity-ETH uses a cryptographically secure pseudorandom number generator (CSPRNG) to generate
-            Ethereum addresses.<br>
             The keystore file is encrypted with a AES-128-CTR cipher using the BKDF2-SHA256 derivation function with 65536 hashing rounds.
         </p>
         <h2>Performance</h2>
         <p>
-            For some reason, the performance of Vanity-ETH can vary a lot from a browser to another.
-            Currently, Chrome provides the best results.<br>
-            Using Vanity-ETH on your phone or tablet will work, but don't expect to reach the speed of a good old computer.
+            Chrome provides the best results.<br>
+            Using Vanity-HPB on your phone or tablet will work, but don't expect to reach the speed of a good old computer.
         </p>
         <h2>Compatibility</h2>
         <p>
-            Any address generated with Vanity-ETH is ERC-20 compatible, which means you can use it for an ICO, an
+            Any address generated with Vanity-HPB is HRC-20 and ERC-20 compatible, which means you can use it for an ICO, an
             airdrop, or just to withdraw your funds from an exchange.<br>
-            The keystore file is 100% compatible with MyEtherWallet, MetaMask, Mist, and geth.
+            The keystore file is 100% compatible with MetaMask, geth, etc.
         </p>
     </div>
 </template>
